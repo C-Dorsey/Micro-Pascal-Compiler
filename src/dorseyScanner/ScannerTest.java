ab879397a185df56b68959a0cf9a4b863105908f
Cohl Dorsey <dorseyc@augsburg.edu>
2019-02-05 12:42:32 -0600

    Created the test program that tests the scanner to make sure that it is taking in the tokens correctly.

---
+++ src/dorseyScanner/ScannerTest.java
@@ -0,0 +1,16 @@
+/**
+ * A Java Program that illustrates reading from a text file and strings
+ * using a scanner class using JUnit testing. This program creates a
+ * StringReader, passing a String as parameter to the StringReader
+ * constructor. It then reads the characters one at a time from the
+ * StringReader class. This program also takes an input file
+ * and creates an input file stream to read from. As long as the
+ * scanner doesn't hit a null token, the scanner prints out any tokens
+ * that are not whitespace tokens or null tokens.
+ *
+ * @author Cohl Dorsey
+ */
+package dorseyScanner;
+
+public class ScannerTest {
+}
