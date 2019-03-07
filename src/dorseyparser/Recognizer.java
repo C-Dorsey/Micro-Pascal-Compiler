807e9b694c1ab121253fd7d234110aa2da8d9d56
Cohl Dorsey <dorseyc@augsburg.edu>
2019-03-07 13:23:07 -0600

    Added instance variables and a constructor to the recognizer that will handle a file or a token.

--- src/dorseyparser/Recognizer.java
+++ src/dorseyparser/Recognizer.java
@@ -17,8 +17,57 @@ import java.util.logging.Logger;
 import dorseyScanner.Scanner;
 import dorseyScanner.Token;
 import dorseyScanner.TokenType;
+import static sun.management.Agent.error;
 
 public class Recognizer
 {
+    private Token lookahead;
+    private Scanner scanner;
+
+    /**
+     * The parser constructor that takes in a file path or text
+     * containing tokens. If isFilename is true, it signifies that
+     * the parser is looking at a file. If false, it signifies that
+     * the parser is looking at a lexeme and not a file.
+     * @param text - file path or text containing tokens to be
+     * recognized.
+     * @param isFilename - boolean expression to  help recognize if it
+     * is a file path or text.
+     */
+    public Recognizer( String text, boolean isFilename)
+    {
+        // If the String object passed in is a file, the following
+        // statement is run.
+        if(isFilename)
+        {
+            FileInputStream fis = null; // Create file object stream
+            try
+            {
+                fis = new FileInputStream(text); // Assign the file to the stream
+            }
+            catch (FileNotFoundException ex)
+            {
+                // Error is thrown if file passed in can't be found
+                error("File Not Found");
+            }
+            InputStreamReader isr = new InputStreamReader(fis); // Pass file into the stream reader
+            scanner = new Scanner(isr); // Pass the reader into the scanner
+        }
+        // If the String object passed in is instead a token
+        else
+        {
+            scanner = new Scanner(new StringReader(text)); // Pass token to scanner
+        }
+        try
+        {
+            //Initializing lookahead by reading a token.
+            lookahead = scanner.nextToken();
+        }
+        catch (IOException ex)
+        {
+            // Error is thrown if not a string.
+            error("Scan error");
+        }
+    }
 
 }
