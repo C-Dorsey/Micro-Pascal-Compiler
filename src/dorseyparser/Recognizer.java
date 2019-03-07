cb970ad6eaee17f03711a1664bc9f4d2a7a05cea
Cohl Dorsey <dorseyc@augsburg.edu>
2019-03-07 12:59:18 -0600

    Created a package for the parser. Then created a recognizer class and addeed a to it a description of what the class does.

---
+++ src/dorseyparser/Recognizer.java
@@ -0,0 +1,13 @@
+/**
+ * A Recognizer program that will recognize tokens for the parser.
+ * This program will take in a file or a token, and using our grammar,
+ * will recognize token types and handles them how they are defined to
+ * be by the grammar of our micro pascal language.
+ */
+
+package dorseyparser;
+
+public class Recognizer
+{
+
+}
