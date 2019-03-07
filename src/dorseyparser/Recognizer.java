a624c180e9b8d16fd2ac28ea31a0da24384ed2a4
Cohl Dorsey <dorseyc@augsburg.edu>
2019-03-07 13:02:26 -0600

    Imported the libraries and packages that I will need in order to run the program.

--- src/dorseyparser/Recognizer.java
+++ src/dorseyparser/Recognizer.java
@@ -7,6 +7,17 @@
 
 package dorseyparser;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import dorseyScanner.Scanner;
+import dorseyScanner.Token;
+import dorseyScanner.TokenType;
+
 public class Recognizer
 {
 
