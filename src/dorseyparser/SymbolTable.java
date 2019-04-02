1ce875e20255ac1f22370889ddce49638a8d0d01
Cohl Dorsey <dorseyc@augsburg.edu>
2019-04-02 12:19:32 -0500

    Initial Symbol Table class that will be used to store tokens along with other various functions.

---
+++ src/dorseyparser/SymbolTable.java
@@ -0,0 +1,16 @@
+/**
+ * This program creates a symbol table that is implemented as a hash map
+ * to store information about every pascal identifier added to the symbol
+ * table. The symbol table will store identifier information such as the
+ * character string (or lexeme), its datatype, and its kind. The kind of
+ * identifier can be a program, variable, array, function, or procedure.
+ *
+ * @author Cohl Dorsey
+ */
+
+
+
+package dorseyparser;
+
+public class SymbolTable {
+}
