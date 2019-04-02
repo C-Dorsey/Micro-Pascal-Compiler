1917fac2d5f9ac32c811de37ca0f262476945cf0
Cohl Dorsey <dorseyc@augsburg.edu>
2019-04-02 12:27:25 -0500

    Added a class to represent symbol data for a token.

--- src/dorseyparser/SymbolTable.java
+++ src/dorseyparser/SymbolTable.java
@@ -8,9 +8,38 @@
  * @author Cohl Dorsey
  */
 
-
-
+import java.util.HashMap;
 package dorseyparser;
 
-public class SymbolTable {
+public class SymbolTable
+{
+    private HashMap<String, SymbolData> symbols = new HashMap<>();
+
+
+    /**
+     * Contains the datatype and information values that are stored
+     * in the hash map.
+     */
+    private class SymbolData
+    {
+        String name;
+        KindEnum kind;
+
+        SymbolData(String name, KindEnum kind)
+        {
+            this.name = name;
+            this.kind = kind;
+        }
+
+        /**
+         * The string displayed for the returned SymbolData.
+         *
+         * @return - returns the SymbolData's name and kind.
+         */
+        @Override
+        public String toString()
+        {
+            return  name + kind;
+        }
+    }
 }
