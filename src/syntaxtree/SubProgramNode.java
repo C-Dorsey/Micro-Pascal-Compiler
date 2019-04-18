03a2bc4e5419e0da0e7fe111a0e2eb68d265e2bf
Cohl Dorsey <dorseyc@augsburg.edu>
2019-04-18 12:39:43 -0500

    Made the class extend subprogram declarations node class.

--- src/syntaxtree/SubProgramNode.java
+++ src/syntaxtree/SubProgramNode.java
@@ -1,4 +1,12 @@
+/**
+ * The subprogram node represents a subprogram node in the syntax tree.
+ *
+ * @author Cohl Dorsey
+ */
+
 package syntaxtree;
 
-public class SubProgramNode {
+public class SubProgramNode extends SubProgramDeclarationsNode
+{
+
 }
