7949e3d5494f396f0cdec0d01eadc855d0c5d3a2
Cohl Dorsey <dorseyc@augsburg.edu>
2019-04-18 13:00:00 -0500

    Created a node class to represent a Statement.

---
+++ src/syntaxtree/StatementNode.java
@@ -0,0 +1,12 @@
+/**
+ * The statement node represents a single statement in Pascal.
+ *
+ * @author Cohl Dorsey
+ */
+
+package syntaxtree;
+
+public class StatementNode extends SyntaxTreeNode
+{
+
+}
