8f8eba8e4ec184f606f39fbe90243b060c0664c4
Cohl Dorsey <dorseyc@augsburg.edu>
2019-04-29 02:42:53 -0500

    Created this TypeEnum class for the compiler to enumerate through the types here for identification when the semantic analyzer needs them.

---
+++ src/dorseyparser/TypeEnum.java
@@ -0,0 +1,14 @@
+/**
+ * This is the TypeEnum class.
+ * This class contains the different possible types for every
+ * Symbol Table identifier.
+ *
+ * @author Cohl Dorsey
+ */
+
+package dorseyparser;
+
+public enum TypeEnum
+{
+    INTEGER_TYPE, REAL_TYPE
+}
