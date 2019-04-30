1ef2d867339f9a50ba93e193e42fd6e9e9c5a95e
Cohl Dorsey <dorseyc@augsburg.edu>
2019-04-30 17:14:47 -0500

    Created code generator class file.

---
+++ src/codegen/CodeGeneration.java
@@ -0,0 +1,17 @@
+/**
+ * This is the CodeGeneration Class file.
+ * Theclass takes in a syntax tree and a symbol table as its input and
+ * returns a string of MIPS assembly language code as its output.
+ * It does this by outlining a blueprint of the assembly code for the
+ * overall program and generating code to fill in the missing parts of
+ * the blueprint using nodes from the syntax tree.
+ *
+ * @author Cohl Dorsey
+ */
+
+package codegen;
+
+public class CodeGeneration
+{
+
+}
