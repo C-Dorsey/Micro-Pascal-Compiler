58d2868bc046ea71db18674db62dc694c95a7e01
Cohl Dorsey <dorseyc@augsburg.edu>
2019-04-30 23:53:03 -0500

    Updated compiler main to reflect the current opperations for code generation.

--- src/compiler/CompilerMain.java
+++ src/compiler/CompilerMain.java
@@ -13,6 +13,7 @@ import dorseyparser.Parser;
 import dorseyparser.SymbolTable;
 import syntaxtree.ProgramNode;
 import analyzer.SemanticAnalyzer;
+import codegen.CodeGeneration;
 import java.io.PrintWriter;
 
 public class CompilerMain
@@ -38,7 +39,28 @@ public class CompilerMain
             sa.analyze();
             if (sa.good())
             {
-                //placeholder for running code generator
+                CodeGeneration codeGen = new CodeGeneration(pn, st);
+                String code = codeGen.genCode();
+                //write to asm file
+
+                int dotIndex = fileName.indexOf(".");
+                /*The file name from the first letter of the file name up
+                to the dot. */
+                String baseFileName = fileName.substring(0, dotIndex);
+                /*Changing the file extension by adding the file extension
+                .table on to the file name. */
+                String fileOutName = baseFileName + ".asm";
+                try
+                {
+                    PrintWriter printWriter = new PrintWriter(fileOutName);
+                    //Writing symbol table toString out to the .table file
+                    printWriter.print(code);
+                    printWriter.close();
+                }
+                catch (Exception e)
+                {
+                    System.out.println("Failed to write to file");
+                }
             }
             else
             {
